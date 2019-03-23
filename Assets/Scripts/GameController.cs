635630c594904f0bbfdc63a7f0d197e8bfe47871
KidSunday <sombersunday@gmail.com>
2019-03-23 16:52:02 -0400

    Added Controllers, Font Folder, Font, Display UI ConsoleView (Blanked)

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+   [System.Serializable]
+
+public class GameController : MonoBehaviour
+{
+    public GameObject cView;
+    private Text consoleView;
+    void Start()
+    {
+       consoleView = cView.GetComponent<Text>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+          //  Debug.Log("Test");
+          //consoleView.text = "Words From Script";
+    }
+}
